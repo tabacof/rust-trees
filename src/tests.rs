8448ebcafb60a433d3d193af9f82e4ac1c640615
Guilherme de Lazari <guilhermelcs@gmail.com>
2023-07-26 17:18:27 -0300

    add integrations tests for accuracy of models

--- src/tests.rs
+++ src/tests.rs
@@ -2,6 +2,12 @@
 mod tests {
     use crate::{trees::RandomForest, *};
 
+    fn assert_greater_than(a: f32, b: f32) {
+        if a <= b {
+            panic!("{} is not greater than {}", a, b);
+        }
+    }
+
     #[test]
     fn test_integration() {
         let train = Dataset::read_csv("datasets/diabetes_train.csv", ",");
@@ -13,84 +19,90 @@ mod tests {
     }
 
     #[test]
-    fn test_all_datasets_dt() {
-        println!("Test 1: toy regression");
-        let train = Dataset::read_csv("datasets/toy_train.csv", ";");
-        let test = Dataset::read_csv("datasets/toy_test.csv", ";");
-        let dt = DecisionTree::train_reg(train, 2, Some(1), Some(42));
-        dt.predict(&test);
-
-        println!("Test 2: one feature regression");
-        let train = Dataset::read_csv("datasets/one_feature_train.csv", ",");
-        let test = Dataset::read_csv("datasets/one_feature_test.csv", ",");
-        let dt = DecisionTree::train_reg(train, 2, Some(1), Some(42));
-        dt.predict(&test);
-
-        println!("Test 3: diabetes regression");
-        let train = Dataset::read_csv("datasets/diabetes_train.csv", ",");
-        let test = Dataset::read_csv("datasets/diabetes_test.csv", ",");
-        let dt = DecisionTree::train_reg(train, 5, Some(1), Some(42));
+    fn decision_tree_titanic() {
+        let (train, test) = read_train_test_dataset("titanic");
+        let dt = DecisionTree::train_clf(train, 5, Some(1), Some(43));
         let pred = dt.predict(&test);
-        println!("R2: {}", r2(&test.target_vector, &pred));
-
-        println!("Test 4: housing regression");
-        let train = Dataset::read_csv("datasets/housing_train.csv", ",");
-        let test = Dataset::read_csv("datasets/housing_test.csv", ",");
-        let dt = DecisionTree::train_reg(train, 5, Some(1), Some(42));
-        let pred = dt.predict(&test);
-        println!("R2: {}", r2(&test.target_vector, &pred));
-
-        println!("Test 5: breast cancer classification");
-        let train = Dataset::read_csv("datasets/breast_cancer_train.csv", ",");
-        let test = Dataset::read_csv("datasets/breast_cancer_test.csv", ",");
-        let dt = DecisionTree::train_clf(train, 5, Some(1), Some(42));
-        let pred = dt.predict(&test);
-        let pred = classification_threshold(&pred, 0.5);
-
-        println!("Accuracy: {}", accuracy(&test.target_vector, &pred),);
-
-        println!("Test 6: Titanic classification");
-        let train = Dataset::read_csv("datasets/titanic_train.csv", ",");
-        let test = Dataset::read_csv("datasets/titanic_test.csv", ",");
-        let dt = DecisionTree::train_clf(train, 5, Some(1), Some(42));
-        let pred = dt.predict(&test);
-        let pred = classification_threshold(&pred, 0.5);
-
-        println!("Accuracy: {}", accuracy(&test.target_vector, &pred),);
+        println!("Accuracy: {}", accuracy(&test.target_vector, &pred));
+        assert_greater_than(accuracy(&test.target_vector, &pred), 0.237);
     }
 
     #[test]
-    fn test_all_datasets_rf() {
-        println!("Test 1: diabetes regression");
-        let train = Dataset::read_csv("datasets/diabetes_train.csv", ",");
-        let test = Dataset::read_csv("datasets/diabetes_test.csv", ",");
+    fn decision_tree_breast_cancer() {
+        let (train, test) = read_train_test_dataset("breast_cancer");
+        let dt = DecisionTree::train_clf(train, 5, Some(1), Some(42));
+        let pred = dt.predict(&test);
+        println!("Accuracy: {}", accuracy(&test.target_vector, &pred));
+        assert_greater_than(accuracy(&test.target_vector, &pred), 0.83);
+    }
+
+    #[test]
+    fn decision_tree_housing() {
+        let (train, test) = read_train_test_dataset("housing");
+        let dt = DecisionTree::train_reg(train, 5, Some(1), Some(42));
+        let pred = dt.predict(&test);
+        println!("R2: {}", r2(&test.target_vector, &pred));
+        assert_greater_than(r2(&test.target_vector, &pred), 0.59);
+    }
+
+    #[test]
+    fn decision_tree_diabeties() {
+        let (train, test) = read_train_test_dataset("diabetes");
+        let dt = DecisionTree::train_reg(train, 5, Some(1), Some(42));
+        let pred = dt.predict(&test);
+        println!("R2: {}", r2(&test.target_vector, &pred));
+        assert_greater_than(r2(&test.target_vector, &pred), 0.30);
+    }
+
+    fn read_train_test_dataset(name: &str) -> (Dataset, Dataset) {
+        let train = "datasets/".to_string() + name + "_train.csv";
+        let train = Dataset::read_csv(&train, ",");
+
+        let test = "datasets/".to_string() + name + "_test.csv";
+        let test = Dataset::read_csv(&test, ",");
+
+        (train, test)
+    }
+
+
+    #[test]
+    fn random_forest_diabetes() {
+        let (train, test) = read_train_test_dataset("diabetes");
         let rf = RandomForest::train_reg(train, 10, Some(5), Some(1), Some(42));
         let pred = rf.predict(&test);
         println!("R2: {}", r2(&test.target_vector, &pred));
+        assert_greater_than(r2(&test.target_vector, &pred), 0.38);
+    }
 
-        println!("Test 2: housing regression");
-        let train = Dataset::read_csv("datasets/housing_train.csv", ",");
-        let test = Dataset::read_csv("datasets/housing_test.csv", ",");
+    #[test]
+    fn random_forest_housing() {
+        let (train, test) = read_train_test_dataset("housing");
         let rf = RandomForest::train_reg(train, 10, Some(5), Some(1), Some(42));
         let pred = rf.predict(&test);
         println!("R2: {}", r2(&test.target_vector, &pred));
+        assert_greater_than(r2(&test.target_vector, &pred), 0.641);
+    }
 
-        println!("Test 3: breast cancer classification");
-        let train = Dataset::read_csv("datasets/breast_cancer_train.csv", ",");
-        let test = Dataset::read_csv("datasets/breast_cancer_test.csv", ",");
+    #[test]
+    fn random_forest_breast_cancer() {
+        let (train, test) = read_train_test_dataset("breast_cancer");
         let rf = RandomForest::train_clf(train, 10, Some(5), Some(1), Some(42));
         let pred = rf.predict(&test);
         let pred = classification_threshold(&pred, 0.5);
 
         println!("Accuracy: {}", accuracy(&test.target_vector, &pred),);
+        assert_greater_than(accuracy(&test.target_vector, &pred), 0.964);
 
-        println!("Test 4: Titanic classification");
-        let train = Dataset::read_csv("datasets/titanic_train.csv", ",");
-        let test = Dataset::read_csv("datasets/titanic_test.csv", ",");
+    }
+
+    #[test]
+    fn random_forest_breast_titanic() {
+        let (train, test) = read_train_test_dataset("titanic");
         let rf = RandomForest::train_clf(train, 10, Some(5), Some(1), Some(42));
         let pred = rf.predict(&test);
         let pred = classification_threshold(&pred, 0.5);
 
         println!("Accuracy: {}", accuracy(&test.target_vector, &pred),);
+        assert_greater_than(accuracy(&test.target_vector, &pred), 0.789);
     }
 }
